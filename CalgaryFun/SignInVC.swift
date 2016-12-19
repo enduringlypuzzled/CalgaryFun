7d12324e3517a00a22229bcd41ab5995f90e4fa0
Steve Mecking <stevo@Steves-MacBook-Air.local>
2016-12-19 08:43:49 -0700

    completed Email-Auth

--- CalgaryFun/SignInVC.swift
+++ CalgaryFun/SignInVC.swift
@@ -7,29 +7,54 @@
 //
 
 import UIKit
+import Firebase
 
 class SignInVC: UIViewController {
 
+   
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
+    
+    @IBAction func signinPressed(_ sender: AnyObject) {
+        
+        if let email = emailField.text, let pwd = passwordField.text {
+            FIRAuth.auth()?.signIn(withEmail: email, password: pwd, completion: { (user, error) in
+                
+                //User Signed in with no problem
+                if error == nil {
+                    print("Steve: Email user authenticated with Firebase")
+                    
+                    /*if let user = user {
+                        //let userData = ["provider": user.providerID]
+                        //self.completeSignIn(id: user.uid, userData: userData)
+                    }*/
+                    
+                } else {
+                    
+                    FIRAuth.auth()?.createUser(withEmail: email, password: pwd, completion: { (user, error) in
+                        if error != nil {
+                            print("Steve: Unable to authenticate with Firebase \(error)")
+                        }else {
+                            print("Steve: Succesfully authenticated with new Firebase user")
+                            /*if let user = user {
+                                //let userData = ["provider": user.providerID]
+                                //self.completeSignIn(id: user.uid, userData: userData)
+                            }*/
+                        }
+                    })
+                }
+                
+            })
+            
+        }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
     }
-    */
 
+   
 }
